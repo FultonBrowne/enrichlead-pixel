dd740e85e33dc7214745af45161ef0d7a717d503
Christian <85686735+innoplusdevs@users.noreply.github.com>
2024-11-28 11:16:44 -0500

    fix: added test logic

--- pixel.js
+++ pixel.js
@@ -1,66 +1,58 @@
-// prettier-ignore
-(()=>{ 
-  function t(t){ 
+(() => {
+  function t(t) {
     console.log("PID:", t);
-    
-    let n="https://us-central1-visitorfit.cloudfunctions.net/handlePixelData", 
-        o=window.location.hostname;
-    
-    var e = document.createElement("script");
-    e.async = true;
-    e.src = n + "?pid=" + t;
-    document.head.appendChild(e);
-
-    fetch("https://api.ipdata.co?api-key=33c71249f49c4fc76a917075a622ab36f32162febc931448cd214d04")
-      .then(response => response.json())
-      .then(ipData => {
+    let n = "https://us-central1-visitorfit.cloudfunctions.net/handlePixelData",
+      o = window.location.hostname,
+      e = document.createElement("script");
+    (e.async = !0), (e.src = n + "?pid=" + t), document.head.appendChild(e);
+    const isTest =
+      new URL(window.location.href).searchParams.get("test") === "true";
+    fetch(
+      "https://api.ipdata.co?api-key=33c71249f49c4fc76a917075a622ab36f32162febc931448cd214d04"
+    )
+      .then((e) => e.json())
+      .then((e) => {
         fetch(n, {
           method: "POST",
-          headers: {"Content-Type": "application/json"},
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             pid: t,
             domain: o,
             path: window.location.pathname,
             referrer: document.referrer,
             userAgent: navigator.userAgent,
-            timestamp: (new Date()).toISOString(),
+            timestamp: new Date().toISOString(),
             pageTitle: document.title,
-            ipData: ipData
+            ipData: e,
+            test: isTest,
+          }),
+        })
+          .then((e) => {
+            if (e.ok) {
+              console.log("✅ Data sent successfully");
+              if (isTest) {
+                alert("Congratulations! Test completed successfully");
+              }
+            } else {
+              console.error("❌ Error in response:", e.statusText);
+            }
           })
-        }).then(response => {
-          if (response.ok) {
-            console.log("✅ Data sent successfully");
-          } else {
-            console.error("❌ Error in response:", response.statusText);
-          }
-        }).catch(error => {
-          console.error("❌ Error:", error);
-        });
-      }).catch(error => {
-        console.error("❌ Error fetching IP data:", error);
+          .catch((e) => {
+            console.error("❌ Error:", e);
+          });
+      })
+      .catch((e) => {
+        console.error("❌ Error fetching IP data:", e);
       });
   }
-
-
-  var e = document.currentScript;
-  var n = e?.dataset?.pid;
-
-
-  if (n) {
-    t(n);
-  } else {
-    n = new URL(e.src).searchParams.get("pid");
-    if (n) {
-      console.log("> debug: using pid from query", n);
-      t(n);
-    } else {
-      document.addEventListener("DOMContentLoaded", function() {
+  var e = document.currentScript,
+    n = e?.dataset?.pid;
+  n
+    ? t(n)
+    : (n = new URL(e.src).searchParams.get("pid"))
+    ? (console.log("> debug: using pid from query", n), t(n))
+    : document.addEventListener("DOMContentLoaded", function () {
         var e = document.getElementById("pixel-js")?.dataset?.pid;
-        if (e) {
-          console.log("> debug: using pid from backup", e);
-          t(e);
-        }
+        e && (console.log("> debug: using pid from backup", e), t(e));
       });
-    }
-  }
 })();
