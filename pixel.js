328001a8c7e1e503fa83392fe9a42ae53101eed5
Christian <85686735+innoplusdevs@users.noreply.github.com>
2024-12-03 10:34:27 -0500

    fix: ipdata request

--- pixel.js
+++ pixel.js
@@ -2,9 +2,9 @@
   function t(t) {
     console.log("PID:", t);
     let o = window.location.hostname,
-      n = new URL(window.location.href).searchParams.get("test") === "true";
+      n = "true" === new URL(window.location.href).searchParams.get("test");
     fetch(
-      "https://api.ipdata.co?api-key=33c71249f49c4fc76a917075a622ab36f32162febc931448cd214d04"
+      "https://api.ipdata.co?api-key=33c71249f49c4fc76a917075a622ab36f32162febc931448cd214d04&fields=ip,city,region,country_name,country_code,asn,company"
     )
       .then((e) => e.json())
       .then((e) => {
@@ -41,11 +41,11 @@
       });
   }
   var e = document.currentScript,
-    n = e?.dataset?.pid;
-  n
-    ? t(n)
-    : (n = new URL(e.src).searchParams.get("pid"))
-    ? (console.log("> debug: using pid from query", n), t(n))
+    o = e?.dataset?.pid;
+  o
+    ? t(o)
+    : (o = new URL(e.src).searchParams.get("pid"))
+    ? (console.log("> debug: using pid from query", o), t(o))
     : document.addEventListener("DOMContentLoaded", function () {
         var e = document.getElementById("pixel-js")?.dataset?.pid;
         e && (console.log("> debug: using pid from backup", e), t(e));
